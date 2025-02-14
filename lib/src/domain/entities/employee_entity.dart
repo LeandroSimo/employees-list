8a450b1ff723232051b3819096963dbd00742d31
LeandroSimo <leo.bt.12@hotmail.com>
2025-02-13 23:05:59 -0300

    feat: [Add EmployeeEntity]

---
+++ lib/src/domain/entities/employee_entity.dart
@@ -0,0 +1,17 @@
+class EmployeeEntity {
+  final String id;
+  final String name;
+  final String job;
+  final String admissionDate;
+  final String phone;
+  final String image;
+
+  EmployeeEntity({
+    required this.id,
+    required this.name,
+    required this.job,
+    required this.admissionDate,
+    required this.phone,
+    required this.image,
+  });
+}
